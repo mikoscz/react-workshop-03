7037e0d90c30ce870f5566cec6bb890ccd7fd34a
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-12-21 16:48:02 +0100

    Add support for quantity in cart

--- src/components/CartDetails/CartDetails.jsx
+++ src/components/CartDetails/CartDetails.jsx
@@ -2,21 +2,31 @@ import { useNavigate } from "react-router-dom";
 import products from "../../data/products";
 import useCart from "../../hooks/useCart";
 
-
 export const CartDetails = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const cart = useCart();
-    let sum = cart.products.reduce((sum, item) => { return sum + item.price }, 0)
-    return (
-        <div> <div>{cart.products.map((item) => {
+  const cart = useCart();
 
-            return <div key={item.id} >{item.title} - ${item.price}<button onClick={() => cart.removeProductFromCart(item)}>x</button> </div>
-        })}</div>
-            {sum}$
-            <button onClick={() => navigate("/shipment")}>Submit</button>
-        </div>
+  let sum = cart.products.reduce((sum, item) => {
+    return sum + item.price * item.quantity;
+  }, 0);
 
-
-    )
-}
+  return (
+    <div>
+      {" "}
+      <div>
+        {cart.products.map((item) => {
+          return (
+            <div key={item.id}>
+              {item.title} - ${item.price} - quantity: {item.quantity}
+              <button onClick={() => cart.removeProductFromCart(item)}>
+                x
+              </button>{" "}
+            </div>
+          );
+        })}
+      </div>
+      {sum}$<button onClick={() => navigate("/shipment")}>Submit</button>
+    </div>
+  );
+};
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,69 +1,77 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
-    import { queryClient } from '../main.jsx'
+import { queryClient } from "../main.jsx";
 import products from "../data/products.js";
 import { useState } from "react";
 
-
 function useCart() {
+  const [shipmentData, setShipmentData] = useState({});
 
-    const [shipmentData, setShipmentData]=useState({})
-
-    const query = useQuery(["carts", 1], async () => {
-        const response = await fetch('http://localhost:3000/carts/1');
-        const cart = await response.json();
-        return cart;
-    });
-    const mutation = useMutation(async (cart) => {
-        const response = await fetch('http://localhost:3000/carts/1', {
-            method: 'PATCH',
-            body: JSON.stringify(cart),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-        const updatedCart = await response.json()
-        return updatedCart;
-
-    }, {
-        onSuccess: () => {
-            queryClient.invalidateQueries(["carts", 1])
+  const query = useQuery(["carts", 1], async () => {
+    const response = await fetch("http://localhost:3000/carts/1");
+    const cart = await response.json();
+    return cart;
+  });
+  const mutation = useMutation(
+    async (cart) => {
+      const response = await fetch("http://localhost:3000/carts/1", {
+        method: "PATCH",
+        body: JSON.stringify(cart),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
         },
-    })
+      });
+      const updatedCart = await response.json();
+      return updatedCart;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["carts", 1]);
+      },
+    }
+  );
 
-    const removeProductFromCart = (removedProduct) => {
-        mutation.mutate(
-            { products: query.data.products.filter(product => product.id !== removedProduct.id) }
-        )
+  const removeProductFromCart = (removedProduct) => {
+    mutation.mutate({
+      products: query.data.products.filter(
+        (product) => product.id !== removedProduct.id
+      ),
+    });
+  };
 
+  const addProductToCart = (newProduct) => {
+    // logic here
+    const productsInCart = query.data.products;
+    const alreadyExists = productsInCart.find(
+      (existingProduct) => existingProduct.id === newProduct.id
+    );
+
+    if (alreadyExists) {
+      alreadyExists.quantity += 1;
+    } else {
+      productsInCart.push({ ...newProduct, quantity: 1 });
     }
 
-    const addProductToCart = (product) => {
-        mutation.mutate(
-            {
-                products: [...query.data.products, product]
-            }
+    mutation.mutate({
+      products: productsInCart,
+    });
+  };
 
-        )
-
-    }
-
-    if (query.isLoading) {
-        return {
-            products: [],
-            removeProductFromCart,
-            addProductToCart,
-            setShipmentData,
-            shipmentData
-        }
-    }
+  if (query.isLoading) {
     return {
-        products: query.data.products,
-        removeProductFromCart,
-        addProductToCart,
-        setShipmentData,
-        shipmentData
-    }
+      products: [],
+      removeProductFromCart,
+      addProductToCart,
+      setShipmentData,
+      shipmentData,
+    };
+  }
+  return {
+    products: query.data.products,
+    removeProductFromCart,
+    addProductToCart,
+    setShipmentData,
+    shipmentData,
+  };
 }
-
 
 export default useCart;
